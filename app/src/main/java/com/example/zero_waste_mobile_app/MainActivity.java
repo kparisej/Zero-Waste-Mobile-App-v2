0cddeba2bb04dc9e50692f8136969be6a6978c2c
theaplanas21 <96705669+theaplanas21@users.noreply.github.com>
2022-03-12 22:32:15 -0500

    homepage app changes
    changes activity_main and mainactivity

--- app/src/main/java/com/example/zero_waste_mobile_app/MainActivity.java
+++ app/src/main/java/com/example/zero_waste_mobile_app/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.zero_waste_mobile_app;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -22,9 +25,43 @@ public class MainActivity extends AppCompatActivity {
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+        //NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+       // NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        //NavigationUI.setupWithNavController(navView, navController);
     }
+
+    public void sendHi(View v) {
+        // Do something in response to button click
+        Button button = (Button) v;
+       // EditText msg = (EditText) v;
+
+        int id = v.getId();
+        if (id == R.id.button) {
+           // window.setText("Throw plastic here in bin A!");
+            ((EditText)findViewById(R.id.window)).setText("Throw plastic here in bin A!");
+        }
+        else if (id == R.id.button2) {
+            //button.setText("Throw paper here in bin B!");
+            ((EditText)findViewById(R.id.window)).setText("Throw paper here in bin B!");
+        }
+        else if (id == R.id.button3) {
+           // button.setText("Throw trash here in bin C!");
+            ((EditText)findViewById(R.id.window)).setText("Throw trash here in bin C!");
+        }
+        else if (id == R.id.button5) {
+           // button.setText("Throw glass here in bin D!");
+            ((EditText)findViewById(R.id.window)).setText("Throw glass here in bin D!");
+        }
+    }
+    public void dfltMsg(View v) {
+        // Do something in response to button click
+        Button button = (Button) v;
+        button.setText("hey");
+    }
+    protected void onClick(View v){
+        sendHi(v);
+        //v.text = "hey";
+   }
+
 
 }
