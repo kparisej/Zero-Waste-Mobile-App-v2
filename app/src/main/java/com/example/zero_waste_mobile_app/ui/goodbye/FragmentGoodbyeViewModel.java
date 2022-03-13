4d481c933e24cd62ed5e545a03211b37b2928b08
Karen Pari <parikaren@gmail.com>
2022-03-12 22:00:52 -0500

    Goodbye-page changes

---
+++ app/src/main/java/com/example/zero_waste_mobile_app/ui/goodbye/FragmentGoodbyeViewModel.java
@@ -0,0 +1,7 @@
+package com.example.zero_waste_mobile_app;
+
+import androidx.lifecycle.ViewModel;
+
+public class FragmentGoodbyeViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/zero_waste_mobile_app/ui/goodbye/fragment_goodbye.java
@@ -0,0 +1,36 @@
+package com.example.zero_waste_mobile_app;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class fragment_goodbye extends Fragment {
+
+    private FragmentGoodbyeViewModel mViewModel;
+
+    public static fragment_goodbye newInstance() {
+        return new fragment_goodbye();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_goodbye_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(FragmentGoodbyeViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
